f299bebc54f6fb4d6483c1bb2fe2e9fe4344f2da
lamyita <50775487+lamyita@users.noreply.github.com>
2020-04-23 22:11:51 +0000

    end working

---
+++ script.js
@@ -0,0 +1,21 @@
+const AIGUILLEHR = document.querySelector("#hour");
+const AIGUILLEMIN = document.querySelector("#minute");
+const AIGUILLESEC = document.querySelector("#second");
+
+function  demarrerLaMontre(){
+    var time = new Date();
+    var hour = time.getHours() % 12;
+    var minute = time.getMinutes();
+    var second = time.getSeconds();
+
+    var hourDeg = (hour * 30 )+ (0.5 * minute);
+     var minuteDeg = (minute * 6) + ( 0.1 * second);
+     var secondeDeg = second * 6;
+
+     AIGUILLEHR.style.transform = 'rotate(' + hourDeg +'deg)';
+     AIGUILLEMIN.style.transform = 'rotate(' + minuteDeg +'deg)';
+     AIGUILLESEC.style.transform = 'rotate(' + secondeDeg + 'deg)';
+
+     var interval = setInterval(demarrerLaMontre, 1000);};
+demarrerLaMontre();
+
